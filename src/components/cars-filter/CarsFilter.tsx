135f5f375eb8a2ef0f7337c2caceee7d796eb10c
Yaswanth Goli <dev.yashu58@hotmail.com>
2022-01-18 19:24:22 +0400

    Added filter functioanlity

--- src/components/cars-filter/CarsFilter.tsx
+++ src/components/cars-filter/CarsFilter.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Card, Form, Button } from 'react-bootstrap';
+import CarFilterModel from '../../models/CarFilterModel';
 
 import { useGetCarColorsQuery, useGetCarManufacturersQuery } from '../../services/carsApi';
+
+type CarsFilterProps = {
+  carFilter: CarFilterModel,
+  setCarFilter: (carFilter: CarFilterModel) => void
+};
 
 const { Body } = Card;
 const { Group, Label, Select } = Form;
 
-const CarsFilter = () => {
+const CarsFilter = ({ carFilter, setCarFilter }: CarsFilterProps) => {
+  const { register, handleSubmit } = useForm<CarFilterModel>();
 
   const { data: colors } = useGetCarColorsQuery('');
 
   const { data: manufacturers } = useGetCarManufacturersQuery('');
 
+  const onSubmit: SubmitHandler<CarFilterModel> = data => setCarFilter(data);
+
   return (
     <>
       <Card>
         <Body>
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <Group className='mb-4'>
               <Label htmlFor='carColors'>Color</Label>
-              <Select id='carColors'>
+              <Select id='carColors' {...register('color')}>
                 <option value=''>All Car Colors</option>
                 {colors?.colors?.map(color => (
                   <option value={color} key={color}>{color}</option>
@@ -29,7 +39,7 @@ const CarsFilter = () => {
 
             <Group className='mb-4'>
               <Label htmlFor='carManufacturers'>Manufacturer</Label>
-              <Select id='carManufacturers'>
+              <Select id='carManufacturers' {...register('manufacturer')}>
                 <option value=''>All manufacturers</option>
                 {manufacturers?.manufacturers?.map(manufacturer => (
                   <option value={manufacturer.name} key={manufacturer.name}>{manufacturer.name}</option>
--- src/components/cars-list/CarsList.tsx
+++ src/components/cars-list/CarsList.tsx
@@ -1,28 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { CarsFilter } from '..';
+import CarFilterModel from '../../models/CarFilterModel';
+import CarQueryParamsModel from '../../models/CarQueryParamsModel';
+import CarsListModel, { CarModel } from '../../models/CarsListModel';
 
 import { useGetListOfCarsQuery } from '../../services/carsApi';
 
 const CarsList = () => {
 
-  const { data: carsList } = useGetListOfCarsQuery({
+  const carQueryParams: CarQueryParamsModel = {
     manufacturer: '',
     color: '',
     sort: 'asc',
     page: 1
+  };
+
+  const carListInitialData = {
+    cars: [],
+    totalCarsCount: 0,
+    totalPageCount: 0
+  };
+
+  const [carFilter, setCarFilter] = useState<CarFilterModel>({
+    color: '',
+    manufacturer: ''
   });
+
+  const [cars, setCars] = useState<CarsListModel>(carListInitialData);
+
+  const { data: carsList, refetch } = useGetListOfCarsQuery({
+    ...carQueryParams, 
+    color: carFilter.color, 
+    manufacturer: carFilter.manufacturer
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [carFilter]);
 
   return (
     <>
       <Row>
         <Col md={4}>
-          <CarsFilter />
+          <CarsFilter carFilter={carFilter} setCarFilter={setCarFilter}/>
         </Col>
         <Col md={8}>
-          <div className='cars-list'>
-            <h3 className='cars-list-heading'>Available Cars</h3>
-            <p className='cars-list-info'>Showing {carsList?.cars?.length} of {carsList?.totalPageCount} results</p>
+          <div className='cars-view'>
+            <h3 className='cars-view-heading'>Available Cars</h3>
+            <p className='cars-view-info'>Showing {carsList?.cars?.length} of {carsList?.totalPageCount} results</p>
+            <div className='cars-view-list'>
+              {carsList?.cars?.map(car => (
+                <div className='cars-view-list-info' key={car.stockNumber}>
+                  <div className='cars-picture'>
+                    <img src={car.pictureUrl} alt='Car' />
+                  </div>
+                  <div className='cars-info'>
+                    <h3>{car.manufacturerName} {car.modelName}</h3>
+                    <p>Stock # {car.stockNumber} - {car.mileage.number} {car.mileage.unit} - {car.fuelType} - {car.color}</p>
+                    <Link to={`/cars/${car.stockNumber}`}>View details</Link>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </Col>
       </Row>

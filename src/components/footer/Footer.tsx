c013ec8c3acf5010ccedb738d9e259402eb37c97
Yaswanth Goli <dev.yashu58@hotmail.com>
2022-01-18 11:11:29 +0400

    Added Header and Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  const date = new Date();
+
+  return (
+    <footer className='footer'>
+      <div className='d-flex justify-content-center align-items-center h-100'>
+        <p className='text-center mb-0'>&copy; AUTO1 Group {date.getFullYear()}</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+import logo from '../../images/logo.png';
+
+const { Brand, Toggle, Collapse } = Navbar;
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Navbar expand='md'>
+        <Container>
+          <Brand href='#home'>
+            <img
+              alt='Auto1 Group'
+              src={logo}
+              height='30'
+              className='d-inline-block align-top'
+            />
+          </Brand>
+          <Toggle aria-controls='basic-navbar-nav' />
+          <Collapse id='basic-navbar-nav' className='justify-content-end'>
+            <Nav>
+              <Nav.Link href='#home'>Purchase</Nav.Link>
+              <Nav.Link href='#home'>My Orders</Nav.Link>
+              <Nav.Link href='#link'>Sell</Nav.Link>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+};
+
+export default Header;
